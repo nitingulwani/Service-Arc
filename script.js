9c6562aae072b5094e0f31533a575f6cce394135
Shivansh <shivansh.agrawal2020@vitbhopal.ac.in>
2023-05-17 22:00:39 +0530

    added javascript

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-var time1=15;
-var downloadTimer=setInterval(function(){
-    if(time1<=0)
-    {
-        clearInterval(downloadTimer);
-        document.getElementsByClassName("timer1").innerHTML="Done";   
+document.addEventListener("DOMContentLoaded", function() {
+    // Function to change the color of the circle
+    function changeColor(circleElement, color) {
+      circleElement.style.backgroundColor = color;
     }
-    else{
-        document.getElementsByClassName("timer1").innerHTML=time1+"Seconds remaining";
-    }
-    time1-=1;
-},1000);
+  
+    // Get the circle elements
+    const circles = document.querySelectorAll('.circle');
+  
+    // Change the color of the circles after 3 seconds
+    setTimeout(() => {
+      changeColor(circles[0], 'green');  // Change color of the first circle to green
+      changeColor(circles[1], 'red');    // Change color of the second circle to red
+      changeColor(circles[2], 'yellow'); // Change color of the third circle to yellow
+    }, 5000); // 3000 milliseconds (3 seconds)
+  });
+  
